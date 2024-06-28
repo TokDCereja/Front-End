f1062c775eae9283bc0e022a4ad54bb66f07db45
Enzzo D' Lucca Blengini e Silva <163877681+DLuCcA13@users.noreply.github.com>
2024-06-28 18:09:08 -0300

    V 1.3

--- TokDCereja/loginSignUp/script.js
+++ TokDCereja/loginSignUp/script.js
@@ -1,11 +1,21 @@
 const container = document.getElementById("container");
 const registerBtn = document.getElementById("register");
 const loginBtn = document.getElementById("login");
+const registerConta = document.getElementById("registerConta");
+const loginConta = document.getElementById("loginConta");
 
 registerBtn.addEventListener("click", () => {
   container.classList.add("active");
 });
 
 loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
+registerConta.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginConta.addEventListener("click", () => {
   container.classList.remove("active");
 });
