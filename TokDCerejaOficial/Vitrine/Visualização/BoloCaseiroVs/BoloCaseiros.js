eb78c2b97e9e32d332a2a6a45241e55d90d126a2
Enzzo D' Lucca Blengini e Silva <163877681+DLuCcA13@users.noreply.github.com>
2024-06-29 15:05:22 -0300

    V 2.0

---
+++ TokDCerejaOficial/Vitrine/Visualização/BoloCaseiroVs/BoloCaseiros.js
@@ -0,0 +1,11 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    let menuButton = document.querySelector('.mobile-menu-icon button');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        menuButton.innerHTML = '<i class="fa-solid fa-bars"></i>'; 
+    } else {
+        menuMobile.classList.add('open');
+        menuButton.innerHTML = '<i class="fa-solid fa-x"></i>'; 
+    }
+}
